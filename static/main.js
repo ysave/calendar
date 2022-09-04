79661d65b27566e461a4130741415461e8794edd
save <nick.c@menisch.ch>
2022-09-04 16:58:52 +0200

    get inputs from form

---
+++ static/main.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('register-form')
+form.addEventListener('sumbit', registerUser)
+
+async function registerUser(event){
+    event.preventDefault()
+    const username = document.getElementById('name').value
+    const password = document.getElementById('password').value
+
+    const result = await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then(res => res.json())
+
+    console.log(result)
+}
+const form = document.getElementById('login-form')
+form.addEventListener('submit', login)
+
+async function login(event) {
+    event.preventDefault()
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+
+    const result = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then((res) => res.json())
+
+    if (result.status === 'ok') {
+        console.log('Got the token: ', result.data)
+        localStorage.setItem('token', result.data)
+        alert('Success')
+    } else {
+        alert(result.error)
+    }
+}
